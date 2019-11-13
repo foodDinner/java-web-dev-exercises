bba8cc3bb5f2418d970559697fde39d8217d6961
foodDinner <specialdelivery@gmail.com>
2019-11-13 16:37:48 -0600

    Studio! Added Ponder class.

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -6,13 +6,26 @@ public class Area {
     public static void main(String[] args) {
         Scanner input;
         double radius;
-        double area;
+//        double area;
 
         input = new Scanner(System.in);
         System.out.println("Enter the radius of a circle: ");
         radius = input.nextDouble();
-        input.close();
+//        input.close();
 //        area = Circle.getArea(radius);
-        System.out.println(Circle.getArea(radius));
+        if (radius < 0) {
+            while (radius < 0) {
+                System.out.println("Enter a positive number.");
+                System.out.println("Enter the radius of a circle: ");
+                radius = input.nextDouble();
+            }
+        }
+
+        if (Double.isNaN(radius)) {
+            System.out.println("Enter a radius.");
+        } else {
+            System.out.println(Circle.getArea(radius));
+
+        }
     }
 }
