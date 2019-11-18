1f378abb1cbc5e8c45863571732da96dfbd121b2
foodDinner <specialdelivery@gmail.com>
2019-11-18 13:53:28 -0600

    class five exercise

--- src/org/launchcode/java/demos/Ponder.java
+++ src/org/launchcode/java/demos/Ponder.java
@@ -8,10 +8,11 @@ public class Ponder {
         HashMap<String, Integer> output = new HashMap<>();
         String phrase = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         String[] charsInPhrase = phrase.split("");
+        List<String> charList = Arrays.asList(charsInPhrase);
 
         for (String character : charsInPhrase) {
             if (!output.containsKey(character)) {
-                int count = Collections.frequency(Arrays.asList(charsInPhrase), character);
+                int count = Collections.frequency(charList, character);
                 output.put(character, count);
             }
         }
